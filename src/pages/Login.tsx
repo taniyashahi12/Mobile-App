567f93f11b38929f25237b143649fe2bf2bf72e8
taniyashahi12 <taniyashahi123.ts@gmail.com>
2022-08-18 16:13:23 +0530

    initial code

---
+++ src/pages/Login.tsx
@@ -0,0 +1,36 @@
+import react from 'react';
+import './Global.css';
+import { IonButton, IonInput, IonPage, IonImg, IonContent } from '@ionic/react';
+
+const Login = () =>{
+	return(
+		<IonPage>
+			<div className="container-padding">
+				  <div className="ion-align-center">
+				  	<div className="image_logo">
+				  		<IonImg className="image_width" src='./assets/images/kuroit-logo.png' />
+				  	</div>
+				  	<div className="heading_login">
+				  		<h1>Hey,<br />Login Now.</h1>
+				  		<p>If you are new / <a href="Sign">Create New</a></p>
+				  	</div>
+				  	<div className="input_ion">
+					  	<form>
+					  		<div className="form_container">
+					  			<label className="label_name">Enter Your Pin</label>
+					  			<IonInput className="form_input" type="password" value="" placeholder="1234"></IonInput>
+					  		</div>
+					  		<div className="register_container">
+					  			<IonButton className="register_button" type="submit" expand="block">Login</IonButton>
+					  		</div>
+					  	</form>
+				  	</div>
+				  </div>
+			</div>
+		</IonPage>
+		);
+};
+
+export default Login;
+
+
---
+++ src/pages/Sign.tsx
@@ -0,0 +1,51 @@
+import react from 'react';
+import './Global.css';
+import { IonIcon, IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonInput } from '@ionic/react';
+
+
+const Sign = () =>{
+	return(
+		<IonPage>
+		<div className="bg-color">
+			<IonHeader>
+		        <IonToolbar>
+		          <IonTitle>
+		          	<div className="header_heading">
+			          	<a href="Login"><IonIcon className="back-icon" name="chevron-back-outline"></IonIcon></a>
+			          	<h1>Sign Up</h1>
+		          	</div>
+		          </IonTitle>
+		        </IonToolbar>
+	      	</IonHeader>
+			<div className="padding-all">
+		      	<IonContent>
+		      		<form>
+			      		<div className="">				  		
+			      			<div className="margin-bottom">
+			      				<label className="label_name">First Name</label>
+					  			<IonInput className="form_input" type="password" value="" placeholder="Lorem"></IonInput>
+					  		</div>
+					  		<div className="margin-bottom">
+					  			<label className="label_name">Last Name</label>
+					  			<IonInput className="form_input" type="password" value="" placeholder="Ipsum"></IonInput>
+					  		</div>
+					  		<div className="margin-bottom">
+					  			<label className="label_name">Email</label>
+					  			<IonInput className="form_input" type="password" value="" placeholder="xyz@kuroit.com"></IonInput>
+					  		</div>
+					  		<div className="margin-bottom">
+					  			<label className="label_name">Enter Your Pin</label>
+					  			<IonInput className="form_input" type="password" value="" placeholder="1234"></IonInput>
+					  		</div>
+					  	</div>
+				  		<div className="register_container">
+				  			<IonButton className="register_button" type="submit" expand="block">Sign Up</IonButton>
+				  		</div>
+					</form>
+		      	</IonContent>
+	      	</div>
+	      	</div>
+		</IonPage>
+		);
+};
+export default Sign;
