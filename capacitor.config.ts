bd8d872e24c39c2c4cb9e9ef6548c24a668adbd2
love <love[at] kuroit.com>
2022-08-19 12:07:47 +0100

    android code

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.kuroit.com',
   appName: 'reportMobileApp',
   webDir: 'build',
   bundledWebRuntime: false
