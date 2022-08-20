9710a80e0c5c9e6b907ec4b5aa4640b3fc7f64f9
love <love[at] kuroit.com>
2022-08-20 10:49:32 +0100

    files changes

--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -1,7 +1,6 @@
 import React, {useEffect} from 'react';
 import './Global.css';
 import { IonPage } from '@ionic/react';
-import { SplashScreen } from '@capacitor/splash-screen';
 
 const Splash = ({}) => {
 	useEffect(() => {
